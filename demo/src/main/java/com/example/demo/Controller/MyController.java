db1f143a14ced0d48466a9c4e8fddb0d28ba700a
mohsen azimi <azimimohsen06@gmail.com>
2020-11-19 16:23:58 +0330

    foldering and first controller

---
+++ demo/src/main/java/com/example/demo/Controller/MyController.java
@@ -0,0 +1,10 @@
+package com.example.demo.Controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping({"/home","/","Home"})
+public class MyController {
+}
