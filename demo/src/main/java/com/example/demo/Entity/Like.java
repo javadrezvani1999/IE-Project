054ba9964042fc3845204db4338f1110d338e756
mohsen azimi <azimimohsen06@gmail.com>
2020-11-20 14:09:43 +0330

    add post Table

---
+++ demo/src/main/java/com/example/demo/Entity/Like.java
@@ -0,0 +1,29 @@
+package com.example.demo.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+//@Entity
+//@Table(name="like")
+public class Like {
+
+    /*
+    public Like() {
+    }
+
+    @Column(name="like_date")
+    private Date like_date;
+
+    public Date getLike_date() {
+        return like_date;
+    }
+
+    public void setLike_date(Date like_date) {
+        this.like_date = like_date;
+    }
+    */
+
+}
