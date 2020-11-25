8895207dda1f31fc05968323e5e679ef1d1063cc
LoginPage <javadrezvani1999@gmail.com>
2020-11-25 19:51:51 +0330

    Start comment table code

---
+++ demo/src/main/java/com/example/demo/Entity/comments.java
@@ -0,0 +1,35 @@
+package com.example.demo.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "comments")
+
+public class comments {
+
+    @Id
+    @Column(name = "text")
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+
+
+
+
+
+    public comments(){
+
+    }
+
+}
+
