9c4591f12a3bc21a21b94f5c7f2ec234abd63a70
LoginPage <javadrezvani1999@gmail.com>
2020-11-25 20:11:20 +0330

    Revert "comment table code finish"
    This reverts commit b6191551ea6fa7aeb5e48210bcd3cd839e2ec694.

--- demo/src/main/java/com/example/demo/Entity/comments.java
+++ demo/src/main/java/com/example/demo/Entity/comments.java
@@ -4,8 +4,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.websocket.ClientEndpoint;
-import java.util.Date;
 
 @Entity
 @Table(name = "comments")
@@ -25,42 +23,11 @@ public class comments {
     }
 
 
-    @Column(name = "post_id")
-    private int post_id;
 
-    public int getPost_id() {
-        return post_id;
-    }
 
-    public void setPost_id(int post_id) {
-        this.post_id = post_id;
-    }
 
-    @Column(name = "user_id")
-    private int user_id;
-
-    public int getUser_id() {
-        return user_id;
-    }
-
-    public void setUser_id(int user_id) {
-        this.user_id = user_id;
-    }
-
-    @Column(name = "date")
-    private Date date;
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
 
     public comments(){
-
-
 
     }
 
