7a9b451f85c5adaa27a0c8ab70f40c19a0e79172
MRJRN <javadrezvani1999@gmail.com>
2020-11-26 16:40:04 +0330

    follow table finish

--- demo/src/main/java/com/example/demo/Entity/follow.java
+++ demo/src/main/java/com/example/demo/Entity/follow.java
@@ -1,4 +1,42 @@
 package com.example.demo.Entity;
 
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "follow")
 public class follow {
+
+
+    follow(){
+
+    }
+
+    @Id
+    @Column(name = "following")
+    private int following;
+
+    public int getFollowing() {
+        return following;
+    }
+
+    public void setFollowing(int following) {
+        this.following = following;
+    }
+    @NotNull
+    @Column(name = "follower")
+    private int follower;
+
+    public int getFollower() {
+        return follower;
+    }
+
+    public void setFollower(int follower) {
+        this.follower = follower;
+    }
 }
