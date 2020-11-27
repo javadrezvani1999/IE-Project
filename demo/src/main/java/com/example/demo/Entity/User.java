d03417e564cf686ace5efad4bf75ca3b43f57e57
MRJRN <javadrezvani1999@gmail.com>
2020-11-27 12:51:48 +0330

    Update User.java

--- demo/src/main/java/com/example/demo/Entity/User.java
+++ demo/src/main/java/com/example/demo/Entity/User.java
@@ -28,7 +28,7 @@ public class User {
     public String getPassword() { return password;  }
     public void setPassword(String password) { this.password = password;  }
 
-
+/*
 
     @Column(name="fname")
     private String fname;
@@ -75,5 +75,7 @@ public class User {
 
     public int getFollowing_num() { return following_num;  }
     public void setFollowing_num(int following_num) { this.following_num = following_num;  }
+
+    */
 
 }
