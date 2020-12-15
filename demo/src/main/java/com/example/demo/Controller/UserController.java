f72d09c32b482dd4e34f7e049c80e4274d276123
MRJRN <javadrezvani1999@gmail.com>
2020-12-15 11:19:20 +0330

    update

--- demo/src/main/java/com/example/demo/Controller/UserController.java
+++ demo/src/main/java/com/example/demo/Controller/UserController.java
@@ -5,6 +5,7 @@ import com.example.demo.Entity.ozvha;
 import com.example.demo.Repository.UserRepository;
 import com.example.demo.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,10 +24,25 @@ public class UserController {
         this.userService = UserService;
     }
 
+
+    @RequestMapping(value =  "" , method = RequestMethod.GET)
+    public String users(Model model){
+        model.addAttribute(userService.FindAllOzvhas());
+        return "ozvha";
+    }
+
+
     @RequestMapping(value = {"/" , ""} , method = RequestMethod.GET)
 
     public List<ozvha> getusers(){
         return this.userService.FindAllOzvhas();
+    }
+
+
+    @RequestMapping(value = "/register" , method = RequestMethod.GET)
+    public String registerpage(Model model){
+        model.addAttribute("ozvha" , new ozvha());
+        return "Signuppage";
     }
 
 
